0f07b48508a6794f067ec63b4f7af9d0701972c5
alextxv-23 <alextxv@correo.ugr.es>
2024-10-04 21:02:47 +0200

    Atributos de instancia y clase implementadas Closes #2

---
+++ MyHayDay/src/modelos/Trigo.java
@@ -0,0 +1,31 @@
+package modelos;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Cristian Alex Torres Velasquez 
+ */
+public class Trigo {
+    //-------------------------------------------------
+    // Atributos
+    //-------------------------------------------------
+    
+    public static final String ICONO = "";
+    public static final int DURACION = 2;
+    public static final int EXPERIENCIA = 1;
+    public static final int PRECIO_VENTA = 2;
+    public static final int RIEGO = 10;
+    
+    public String icono;
+    private int duracionProduccion;
+    private int experienciaRecolecta;
+    private int precioVenta;
+    private int riego;
+    
+    
+    
+}
