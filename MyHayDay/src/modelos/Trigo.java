7f0dcdca7e04ddbb71aee37907f7951b1e82f84b
alextxv-23 <alextxv@correo.ugr.es>
2024-10-06 12:36:04 +0200

    Clase trigo base implementado. Closes #1 Getter y Setters implementados. Closes #3 Constructores implementados. Closes #4 Test construcción de trigos. Closes #5 Test obtención de trigos. Closes #6

--- MyHayDay/src/modelos/Trigo.java
+++ MyHayDay/src/modelos/Trigo.java
@@ -14,18 +14,76 @@ public class Trigo {
     // Atributos
     //-------------------------------------------------
     
-    public static final String ICONO = "";
+    public static final String ICONO = "./img/icono.png";
     public static final int DURACION = 2;
     public static final int EXPERIENCIA = 1;
     public static final int PRECIO_VENTA = 2;
     public static final int RIEGO = 10;
     
-    public String icono;
+    private String icono;
     private int duracionProduccion;
     private int experienciaRecolecta;
     private int precioVenta;
     private int riego;
+
+    // Constructor con todos los parametros
+    public Trigo(String icono, int duracionProduccion, int experienciaRecolecta, int precioVenta, int riego) {
+        this.icono = icono;
+        this.duracionProduccion = duracionProduccion;
+        this.experienciaRecolecta = experienciaRecolecta;
+        this.precioVenta = precioVenta;
+        this.riego = riego;
+    }
+
+    // Constructor por defecto
+    public Trigo() {
+        this(ICONO, DURACION, EXPERIENCIA, PRECIO_VENTA, RIEGO);
+    }
+
+    //Constructor copia
+    public Trigo(Trigo otro){
+        this(otro.icono, otro.duracionProduccion, otro.experienciaRecolecta, otro.precioVenta, otro.riego);
+    }
     
+    public String getIcono() {
+        return icono;
+    }
+
+    public void setIcono(String icono) {
+        this.icono = icono;
+    }
+
+    public int getDuracionProduccion() {
+        return duracionProduccion;
+    }
+
+    public void setDuracionProduccion(int duracionProduccion) {
+        this.duracionProduccion = duracionProduccion;
+    }
+
+    public int getExperienciaRecolecta() {
+        return experienciaRecolecta;
+    }
+
+    public void setExperienciaRecolecta(int experienciaRecolecta) {
+        this.experienciaRecolecta = experienciaRecolecta;
+    }
+
+    public int getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(int precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public int getRiego() {
+        return riego;
+    }
+
+    public void setRiego(int riego) {
+        this.riego = riego;
+    }
     
     
 }
--- MyHayDay/src/myhayday/MyHayDay.java
+++ MyHayDay/src/myhayday/MyHayDay.java
@@ -4,6 +4,8 @@
  */
 package myhayday;
 
+import modelos.Trigo;
+
 /**
  *
  * @author Cristian Alex Torres Velásquez
@@ -15,6 +17,37 @@ public class MyHayDay {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // Test de constructor paramétrico
+        Trigo trigoParametrico = new Trigo("aun no tiene", 10, 5, 2, 1);
+        
+        // Test de constructor por defecto
+        Trigo trigoDefecto = new Trigo();
+        
+        // Test de constructor copia
+        Trigo trigoCopia = new Trigo(trigoParametrico);
+        
+        // Test getters de trigo paramétrico
+        System.out.println("Trigo paramétrico:\n------------------\nIcono: " + trigoParametrico.getIcono()
+        + "\nDuración producción: " + trigoParametrico.getDuracionProduccion()
+        + "\nExperiencia de recolección: " + trigoParametrico.getExperienciaRecolecta()
+        + "\nPrecio de venta: " + trigoParametrico.getExperienciaRecolecta()
+        + "\nRiego: " + trigoParametrico.getRiego());
+        
+        // Test getters de trigo por defecto
+        System.out.println("\nTrigo por defecto:\n------------------\nIcono: " + trigoDefecto.getIcono()
+        + "\nDuración producción: " + trigoDefecto.getDuracionProduccion()
+        + "\nExperiencia de recolección: " + trigoDefecto.getExperienciaRecolecta()
+        + "\nPrecio de venta: " + trigoDefecto.getExperienciaRecolecta()
+        + "\nRiego: " + trigoDefecto.getRiego());
+        
+        // Test getters de trigoCopia
+        System.out.println("\nTrigo copia :\n-------------\nIcono: " + trigoCopia.getIcono()
+        + "\nDuración producción: " + trigoCopia.getDuracionProduccion()
+        + "\nExperiencia de recolección: " + trigoCopia.getExperienciaRecolecta()
+        + "\nPrecio de venta: " + trigoCopia.getExperienciaRecolecta()
+        + "\nRiego: " + trigoCopia.getRiego());
+        
     }
     
 }
